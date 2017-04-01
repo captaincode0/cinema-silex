5b4ca3beff9bbc3f46cbbb4a62bd5ff185ccf96d
AHM77777 <ahm@lincolnlaw.com>
2017-04-01 12:57:29 -0600

    cinema-silex by AHM: Added account recovery tables

--- db/db.sql
+++ db/db.sql
@@ -29,11 +29,24 @@ create table accountactivation (
 	userid int unsigned not null,
 	token char(32) null,
 	expired boolean not null default false
-)engine=innodb; 
+)engine=innodb;
 
 alter table accountactivation
 	add constraint fk_users0
 		foreign key accountactivation (userid)
+			references users (id)
+				on update no action
+				on delete cascade;
+
+create table accountrecovery (
+	userid int unsigned not null,
+	token char(32) null,
+	expired boolean not null default false
+)engine=innodb;
+
+alter table accountrecovery
+	add constraint fk_users1
+		foreign key accountrecovery (userid)
 			references users (id)
 				on update no action
 				on delete cascade;
