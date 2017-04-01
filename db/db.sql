ee827df6f76b90b05c0e78c8549ea32c2d5ed35c
captaincode0 <developerdiego0@gmail.com>
2017-04-01 12:44:15 -0600

    Added database

---
+++ db/db.sql
@@ -0,0 +1,39 @@
+create cinemadb;
+use cinemadb;
+
+/**
+ * Users
+ * Items
+ * Tickets
+ * Movies
+ * Schedule
+ */
+
+/**
+ * @table: users
+ * Types:
+ * 		0) Admin
+ * 		1) Guest
+ */
+
+create table users (
+	id int unsigned not null primary key auto_increment,
+	email varchar(50) unique not null,
+	name varchar(45) not null,
+	passwd char(32) not null,
+	usertype boolean not null,
+	isactive boolean not null default false
+)engine=innodb;
+
+create table accountactivation (
+	userid int unsigned not null,
+	token char(32) null,
+	expired boolean not null default false
+)engine=innodb; 
+
+alter table accountactivation
+	add constraint fk_users0
+		foreign key accountactivation (userid)
+			references users (id)
+				on update no action
+				on delete cascade;
