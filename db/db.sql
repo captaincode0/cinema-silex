7d75c93a7cb6e6127dd4ba39d3a303afd0922845
captaincode0 <developerdiego0@gmail.com>
2017-04-01 13:22:18 -0600

    changes by captaincode0, Added more tables in base model

--- db/db.sql
+++ db/db.sql
@@ -50,3 +50,43 @@ alter table accountrecovery
 			references users (id)
 				on update no action
 				on delete cascade;
+
+create table movies (
+	id int unsigned not null primary key auto_increment,
+	name varchar(80) not null,
+	descr varchar(150) not null,
+	lenght int not null
+)engine=innodb;
+
+create table movieschedule(
+	movieid int unsigned not null,
+	ihour time not null,
+	ehour time not null,
+	day enum("mon", "thu", "wed", "thur", "fry", "sat", "sun") not null
+)engine=innodb;
+
+alter table movieschedule
+	add constraint fk_movies0
+		foreign key movieschedule (movieid)
+			references movies (id)
+				on update no action
+				on delete cascade;
+
+create table items (
+	id int unsigned not null primary key auto_increment,
+	name varchar(60) not null,
+	price float not null
+)engine=innodb;
+
+create table buyitems (
+	itemid int unsigned not null,
+	quantity int not null
+)engine=innodb;
+
+alter table buyitems
+	add constraint fk_items0
+		foreign key buyitems (itemid)
+			references items (id)
+				on update no action
+				on delete cascade;
+
